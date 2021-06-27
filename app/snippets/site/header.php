f3566c28fcc3b09cf811714abe73fce6fc2793b0
Pasha Frolov <pfrlvgg@gmail.com>
2021-06-27 16:44:30 +0300

    Start 🥳

---
+++ app/snippets/site/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <?= snippet('site/seo') ?>
+  <?= snippet('site/icons') ?>
+
+  <?= css('https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css') ?>
+
+
+  <?php if (!option('debug') and $site->scrips_head()->isNotEmpty()) : ?>
+    <?= $site->scrips_head() ?>
+  <?php endif; ?>
+</head>
+
+<body>

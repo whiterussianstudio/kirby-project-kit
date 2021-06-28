6cb0dda04e9de336947623813eff233736aaae74
Pasha Frolov <pfrlvgg@gmail.com>
2021-06-28 18:13:50 +0300

    refactor: add default page styles

--- app/snippets/site/header.php
+++ app/snippets/site/header.php
@@ -9,11 +9,14 @@
   <?= snippet('site/seo') ?>
   <?= snippet('site/icons') ?>
 
-  <?= mix('/css/index.css') ?>
+  <?= css('https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css') ?>
 
+  <?php /* ?>
+  <?= mix('/css/index.css') ?>
   <?= mix('/js/manifest.js', ['defer' => true]) ?>
   <?= mix('/js/vendor.js', ['defer' => true]) ?>
   <?= mix('/js/index.js', ['defer' => true]) ?>
+  <?php */ ?>
 
   <?php if (!option('debug') and $site->scrips_head()->isNotEmpty()) : ?>
     <?= $site->scrips_head() ?>
